cd2ebc6e8b9865959d0d492b73566494a45a4819
dheeraj <jdeeran2004@gmail.com>
2024-11-24 15:45:18 +0530

    update login.jsx

--- Bug-Tracking-Application/client/src/pages/Login.jsx
+++ Bug-Tracking-Application/client/src/pages/Login.jsx
@@ -1,24 +1,45 @@
 import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useActionData } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Login";
 import { FormRow } from "../components";
 import customFetch from "../utils/customFetch";
 
+// export const action = async ({ request }) => {
+//   const formData = await request.formData();
+//   const data = Object.fromEntries(formData);
+
+//   try {
+//     await customFetch.post("/auth/login", data);
+//     toast.success("Registration successful");
+//     return redirect("/dashboard");
+//   } catch (error) {
+//     console.log(error);
+//     toast.error(error?.response?.data?.message);
+//     return error;
+//   }
+// };
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = { msg: "" };
+  if (data.password.length < 3) {
+    errors.msg = "password too short";
+    return errors;
+  }
   try {
     await customFetch.post("/auth/login", data);
-    toast.success("Registration successful");
+    toast.success("Login successful");
     return redirect("/dashboard");
   } catch (error) {
-    console.log(error);
-    toast.error(error?.response?.data?.msg);
-    return error;
+    // toast.error(error?.response?.data?.msg);
+    errors.msg = error?.response?.data?.message;
+    return errors;
   }
 };
 
 const Login = () => {
+  const errors = useActionData();
   const navigation = useNavigation();
   console.log(navigation);
   const isSubmitting = navigation.state === "submitting";
@@ -28,6 +49,7 @@ const Login = () => {
         <h4>login</h4>
         <FormRow type="email" name="username" />
         <FormRow type="password" name="password" />
+        {errors && <p style={{ color: "red" }}>{errors.msg}</p>}
         <button type="submit" className="btn btn-block" disabled={isSubmitting}>
           {isSubmitting ? "submitting..." : "submit"}
         </button>
